25974044bc5bc8170ad2f14b8b3506297c461464
Aishani Pandey <93460154+AishaniPandey@users.noreply.github.com>
2024-06-22 16:58:09 +0530

    Added social media links and ran npm run build

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -19,8 +19,10 @@ const Footer = () => {
           <p>Email: highlandhistories@gmail.com</p>
         </section>
         <section className="footer-section">
-          <h4>Follow Us</h4>
-          <p>Social media links here</p>
+          <h4>Follow Us On:</h4>
+          <p> Facebook- https://www.facebook.com/groups/866531951968215 </p>
+          <p> Instagram- https://www.instagram.com/highlandhistories </p>
+          <p> X- https://x.com/MountainMatters/status/1803831902731706763?t=bi7kQJHIUSt8kcWLoZ4oMg&s=08 </p>
         </section>
       </div>
       <div className="footer-bottom">
