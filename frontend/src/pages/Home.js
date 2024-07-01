6446fdc1cffc4f331d456eb6b730ffb125044146
Samagra Bharti <127509973+argamas23@users.noreply.github.com>
2024-07-01 18:16:24 +0530

    Full Background

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -49,17 +49,31 @@ const Home = () => {
     }
 
         return (
+            // <div className="container">
+            //     <h1>Highland Histories</h1>
+            //     <h2>Digital Archives</h2>
+            //     {imagePaths.length > 0 && <img src={imagePaths[currentImage]} alt="Mountain Archive" style={{ width: '100%', height: 'auto' }} />}
+            //     <p>
+            //         Mountains are not merely geographically distinct, the human societies they nurtured are markedly different from those which emerged in the plains. This research theme works to centre mountain societies from the margins of social theory and historical practice. It draws on the works of Lucien Febvre, Owen Lattimore, Shekhar Pathak, Chetan Singh, Willem van Schendel, James Scott, and Aniket Alam, among other scholars of mountain societies to build conceptual tools, methodologies, and social theory for the study of mountain societies.
+            //     </p>
+            //     <p>
+            //         This is called a Lab because the intention is to, one, test social science and humanities theories for the specificities of mountain societies and, two, integrate computational tools like GIS, NLP, Data Analytics and Machine Learning into our research methods and theories.
+            //     </p>
+            // </div>
             <div className="container">
-                <h1>Highland Histories</h1>
-                <h2>Digital Archives</h2>
-                {imagePaths.length > 0 && <img src={imagePaths[currentImage]} alt="Mountain Archive" style={{ width: '100%', height: 'auto' }} />}
-                <p>
-                    Mountains are not merely geographically distinct, the human societies they nurtured are markedly different from those which emerged in the plains. This research theme works to centre mountain societies from the margins of social theory and historical practice. It draws on the works of Lucien Febvre, Owen Lattimore, Shekhar Pathak, Chetan Singh, Willem van Schendel, James Scott, and Aniket Alam, among other scholars of mountain societies to build conceptual tools, methodologies, and social theory for the study of mountain societies.
-                </p>
-                <p>
-                    This is called a Lab because the intention is to, one, test social science and humanities theories for the specificities of mountain societies and, two, integrate computational tools like GIS, NLP, Data Analytics and Machine Learning into our research methods and theories.
-                </p>
+            <div className="background-image" style={{ backgroundImage: `url(${imagePaths[currentImage]})` }}>
+                <div className="content">
+                    <h1>Highland Histories</h1>
+                    <h2>Digital Archives</h2>
+                    <p>
+                        Mountains are not merely geographically distinct, the human societies they nurtured are markedly different from those which emerged in the plains. This research theme works to centre mountain societies from the margins of social theory and historical practice. It draws on the works of Lucien Febvre, Owen Lattimore, Shekhar Pathak, Chetan Singh, Willem van Schendel, James Scott, and Aniket Alam, among other scholars of mountain societies to build conceptual tools, methodologies, and social theory for the study of mountain societies.
+                    </p>
+                    <p>
+                        This is called a Lab because the intention is to, one, test social science and humanities theories for the specificities of mountain societies and, two, integrate computational tools like GIS, NLP, Data Analytics and Machine Learning into our research methods and theories.
+                    </p>
+                </div>
             </div>
+        </div>
         );
 };
 
