612fd085db1820151b2950a3728869a233ac4ce5
Samagra Bharti <127509973+argamas23@users.noreply.github.com>
2024-12-10 00:01:04 +0530

    Home Page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,21 +1,14 @@
-// src/pages/Home.js
 import React, { useState, useEffect } from 'react';
 import './Home.css'; // Import CSS file
 
 const Home = () => {
     const [currentImage, setCurrentImage] = useState(0);
-    // Update the path to the correct one
     const images = require.context('../images/home', false, /\.(png|jpe?g|svg|heic)$/);
 
-    console.log(images.keys());
     const imagePaths = images.keys().map(image => {
         const path = images(image);
-        console.log('Image:', image, 'Resolved Path:', path);
         return path.default || path;
     });
-
-    // Log the imagePaths array to verify
-    console.log(imagePaths);
 
     useEffect(() => {
         const intervalId = setInterval(() => {
@@ -29,33 +22,45 @@ const Home = () => {
         return <div>Loading images...</div>;
     }
 
-        return (
-          <div className="home-container">
-          <div className="image-container">
-            {imagePaths.map((path, index) => (
-              <img
-                key={index}
-                src={path}
-                alt={`Mountain Archive ${index + 1}`}
-                className={`carousel-image ${index === currentImage ? 'active' : ''}`}
-              />
-            ))}
-          </div>
-          <div className="home-overlay">
-            <h1>Highland Histories</h1>
-            <h2>Digital Archives</h2>
-            <div className="home-content">
-              <p id="home-para">
-                Mountains are not merely geographically distinct, the human societies they nurtured are markedly different from those which emerged in the plains. This research theme works to centre mountain societies from the margins of social theory and historical practice. It draws on the works of Lucien Febvre, Owen Lattimore, Shekhar Pathak, Chetan Singh, Willem van Schendel, James Scott, and Aniket Alam, among other scholars of mountain societies to build conceptual tools, methodologies, and social theory for the study of mountain societies.
-              </p>
-              <p id="home-para">
-                This is called a Lab because the intention is to, one, test social science and humanities theories for the specificities of mountain societies and, two, integrate computational tools like GIS, NLP, Data Analytics and Machine Learning into our research methods and theories.
-              </p>
+    return (
+        <div className="home-container">
+            <div className="image-container">
+                {imagePaths.map((path, index) => (
+                    <img
+                        key={index}
+                        src={path}
+                        alt={`Mountain Archive ${index + 1}`}
+                        className={`carousel-image ${index === currentImage ? 'active' : ''}`}
+                    />
+                ))}
             </div>
-          </div>
+            <div className="home-overlay">
+                <h1>Highland Histories</h1>
+                <h2><em>Making Mountains Matter</em></h2>
+
+                <div className="home-content">
+                    <p id="home-para">
+                        Highland Histories is the home for research, thinking, knowing, and engaging with mountains and their societies. We will host primary sources – visual, oral, textual – in our archives and build a library of on-going research and other resources.
+                    </p>
+                    <p id="home-para">
+                        At present Highland Histories is based out of Aniket Alam’s lab at IIIT Hyderabad. We hope to build a network of collaborators across the world who work on, and in, the highlands. We hope to grow this community of fellow travellers with nodes and hubs establishing partnerships and friendships across different regions, disciplines, and methods.
+                    </p>
+                    <p id="home-para">
+                        What unites us is our perspective. Mountains have always fascinated people and continue to do so. Mountains and their worlds are seen as other-worldly geographies, realms magical, and zones of spiritual awakening. In this perspective – whether for the tourist, the researcher, or the political agent – mountains are condemned to remain the golden other of the mainstream. A liminal space, mountains open doors to alternate worlds which are the periphery of civilisation, whether ennobling or barbaric. Highlands and the people who live here thus remain perennial recipients of the altruistic or the civilising gaze, depending on preferences.
+                    </p>
+                    <p id="home-para">
+                        Our perspective centres the Highland and its Histories. We view the mountains as a specific social formation with its own particular ways of organising lives and livelihoods, spaces and sensibilities, ecologies and economies. Conceptual tools and social theory which have emerged from the historical experience of the plains-societies cannot explain highlands and their histories, unless they are recalibrated ground up, without the hard work of rethinking them along the material realities of mountains.
+                    </p>
+                    <p id="home-para">
+                        Highland Histories is an effort to do just that: bring people and ideas together to centre mountains and their societies through their own histories; to understand and explain mountain societies in their own terms, based on the foregrounding of the agency of highland people.
+                    </p>
+                    <p id="home-para">
+                        Mountains are not merely geographically distinct, the human societies they nurtured are markedly different from those which emerged in the plains. Highland Histories works to centre mountain societies from the margins of social theory and historical practice. It draws on the works of Lucien Febvre, Owen Lattimore, Shekhar Pathak, Chetan Singh, Willem van Schendel, James Scott, and Aniket Alam, among other scholars of mountain societies to build conceptual tools, methodologies, and social theory for the study of mountain societies.
+                    </p>
+                </div>
+            </div>
         </div>
-        
-        );
+    );
 };
 
 export default Home;
